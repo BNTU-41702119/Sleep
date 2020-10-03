9c7ddfd60375704bdf409334c5100526cbab1f6b
Artem <artyom.balamutyuk1@gmail.com>
2020-10-03 19:27:52 +0300

    Add sleep functions

--- Sleep.cpp
+++ Sleep.cpp
@@ -1,20 +1,26 @@
-// Sleep.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <thread>
+#include <chrono>
+#include <string>
 #include <iostream>
+
+using namespace std;
+
+void sleep(int seconds)
+{
+    this_thread::sleep_for(chrono::seconds(seconds));
+}
+
+void sleep(string seconds)
+{
+    this_thread::sleep_for(chrono::seconds(stoi(seconds)));
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    sleep(3);
+    cout << "Hello World!\n";
+    sleep("3");
+    cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
